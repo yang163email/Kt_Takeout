2c9d6aa4e1695a1f10e899e8893fa5cf36aa7c1b
yanganan <13640662396@163.com>
2017-11-29 21:50:34 +0800

    多类型条目支持

--- app/src/main/java/com/yan/takeout/ui/adapter/HomeRvAdapter.kt
+++ app/src/main/java/com/yan/takeout/ui/adapter/HomeRvAdapter.kt
@@ -13,6 +13,11 @@ import com.yan.takeout.ui.views.HomeCommonItemView
  */
 class HomeRvAdapter(val context: Context): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
+    companion object {
+        val TYPE_TITLE = 0
+        val TYPE_SELLER = 1
+    }
+
     private val mDatas = mutableListOf<String>()
 
     fun setDatas(datas: List<String>) {
@@ -20,15 +25,35 @@ class HomeRvAdapter(val context: Context): RecyclerView.Adapter<RecyclerView.Vie
         notifyDataSetChanged()
     }
 
+    override fun getItemViewType(position: Int): Int =
+        if (position == 0) TYPE_TITLE
+        else TYPE_SELLER
+
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
-        val homeCommonItemView = holder?.itemView as HomeCommonItemView
-        homeCommonItemView.bindView(mDatas[position])
+        when (getItemViewType(position)) {
+            TYPE_TITLE -> {
+                val homeCommonItemView = holder?.itemView as HomeCommonItemView
+                homeCommonItemView.bindView("我是大哥---------------------")
+            }
+            TYPE_SELLER -> {
+                val homeCommonItemView = holder?.itemView as HomeCommonItemView
+                homeCommonItemView.bindView(mDatas[position-1])
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder =
-        HomeRvViewHolder(HomeCommonItemView(context))
+        when (viewType) {
+            TYPE_TITLE -> HomeTitleViewHolder(HomeCommonItemView(context))
+            TYPE_SELLER -> HomeSellerViewHolder(HomeCommonItemView(context))
+            else -> HomeTitleViewHolder(HomeCommonItemView(context))
+        }
 
-    override fun getItemCount(): Int = mDatas.size
+    override fun getItemCount(): Int =
+        if ((mDatas.size > 0)) mDatas.size + 1
+        else 0
 
-    class HomeRvViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView)
+    class HomeTitleViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView)
+
+    class HomeSellerViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView)
 }
