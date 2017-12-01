d521382466b71d6601e833878fa84850ca27f98d
yanganan <13640662396@163.com>
2017-12-01 12:50:45 +0800

    提交验证码的实现

--- app/src/main/java/com/yan/takeout/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/yan/takeout/ui/activity/LoginActivity.kt
@@ -30,6 +30,9 @@ class LoginActivity: AppCompatActivity() {
                 if (event == SMSSDK.EVENT_GET_VERIFICATION_CODE) {
                     // 这里是验证成功的回调，可以处理验证成功后您自己的逻辑，需要注意的是这里不是主线程
                     Log.d(TAG, "afterEvent: 获取验证码成功")
+                } else if (event == SMSSDK.EVENT_SUBMIT_VERIFICATION_CODE) {
+                    //提交验证码成功
+                    Log.d(TAG, "afterEvent: 提交验证码成功")
                 }
             }
         }
@@ -49,6 +52,11 @@ class LoginActivity: AppCompatActivity() {
         tv_user_code.setOnClickListener {
             val phoneNum = et_user_phone.text.toString().trim()
             SMSSDK.getVerificationCode("86", phoneNum)
+        }
+        iv_login.setOnClickListener {
+            val phoneNum = et_user_phone.text.toString().trim()
+            val code = et_user_code.text.toString().trim()
+            SMSSDK.submitVerificationCode("86", phoneNum, code)
         }
     }
 
