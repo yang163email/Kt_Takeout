9de53cbc23517be161e68361ade2b9ab5d723553
yanganan <13640662396@163.com>
2017-12-06 11:43:20 +0800

    移动地图和周边搜索

--- app/src/main/java/com/yan/takeout/kt/ui/activity/MapLocationActivity.kt
+++ app/src/main/java/com/yan/takeout/kt/ui/activity/MapLocationActivity.kt
@@ -7,10 +7,20 @@ import android.support.v13.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import com.amap.api.location.AMapLocation
 import com.amap.api.location.AMapLocationClient
+import com.amap.api.maps2d.AMap
+import com.amap.api.maps2d.CameraUpdateFactory
 import com.amap.api.maps2d.MapView
+import com.amap.api.maps2d.model.LatLng
+import com.amap.api.services.core.LatLonPoint
+import com.amap.api.services.core.PoiItem
+import com.amap.api.services.poisearch.PoiResult
+import com.amap.api.services.poisearch.PoiSearch
 import com.yan.takeout.kt.R
 import org.jetbrains.anko.toast
+
+
 
 
 /**
@@ -18,18 +28,32 @@ import org.jetbrains.anko.toast
  *  @date        : 2017/12/5 19:43
  *  @description : 地图定位界面
  */
-class MapLocationActivity : AppCompatActivity() {
+class MapLocationActivity : AppCompatActivity(), PoiSearch.OnPoiSearchListener {
+    override fun onPoiItemSearched(poiItem: PoiItem?, rCode: Int) {
+
+    }
+
+    override fun onPoiSearched(poiResult: PoiResult?, rCode: Int) {
+        if (rCode == 1000) {
+            //成功
+            poiResult?.let {
+                val poiItems = it.pois
+                toast("一共有${poiItems.size}个兴趣点")
+            }
+        }
+    }
 
     //声明AMapLocationClient类对象
     lateinit var mLocationClient: AMapLocationClient
 
+    lateinit var aMap: AMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map_location)
 
         val mapView = findViewById<View>(R.id.map) as MapView
         mapView.onCreate(savedInstanceState)// 此方法必须重写
-        val aMap = mapView.map
+        aMap = mapView.map
 
         checkPermission()
     }
@@ -66,10 +90,28 @@ class MapLocationActivity : AppCompatActivity() {
         mLocationClient.setLocationListener {
             it?.let {
                 toast(it.address)
+                //移动到当前位置
+                aMap.moveCamera(CameraUpdateFactory.changeLatLng(LatLng(it.latitude, it.longitude)))
+                aMap.moveCamera(CameraUpdateFactory.zoomTo(17f))
                 mLocationClient.stopLocation()
+                doSearchQuery(it)
             }
         }
         //启动定位
         mLocationClient.startLocation()
+    }
+
+    private fun doSearchQuery(aMapLocation: AMapLocation) {
+        val query = PoiSearch.Query("", "", aMapLocation.city)
+        //keyWord表示搜索字符串，
+        //第二个参数表示POI搜索类型，二者选填其一，选用POI搜索类型时建议填写类型代码，码表可以参考下方（而非文字）
+        //cityCode表示POI搜索区域，可以是城市编码也可以是城市名称，也可以传空字符串，空字符串代表全国在全国范围内进行搜索
+        query.pageSize = 30// 设置每页最多返回多少条poiitem
+        query.pageNum = 1//设置查询页码
+
+        val poiSearch = PoiSearch(this, query)
+        poiSearch.bound = PoiSearch.SearchBound(LatLonPoint(aMapLocation.latitude, aMapLocation.longitude), 200)
+        poiSearch.setOnPoiSearchListener(this)
+        poiSearch.searchPOIAsyn()
     }
 }
